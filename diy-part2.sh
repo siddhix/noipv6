4f9c9356b7e53c2b69d6321d1c462fa8e3386266
siddhix <41867661+siddhix@users.noreply.github.com>
2026-06-07 23:04:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,18 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# 1. 斩草除根：抹除固件首次开机时自动生成的 wan6 接口配置
+sed -i '/wan6/,+4d' package/base-files/files/bin/config_generate
+
+# 2. 抹除出厂默认的全局 IPv6 ULA 前缀（彻底消除概览页面的 IPv6 ULA 提示）
+sed -i '/ula_prefix/d' package/base-files/files/bin/config_generate
+
+# 3. 修复 PPPoE 拨号残留漏洞：防止拨号成功后虚假显示空的 IPv6 信息
+sed -i 's/ipv6-up-script/#ipv6-up-script/g' package/network/services/ppp/files/ppp.sh
+sed -i 's/ipv6-down-script/#ipv6-down-script/g' package/network/services/ppp/files/ppp.sh
+
+# 4. 降维打击：在系统内核层面直接下达禁令（开机自动彻底关闭内核 IPv6 协议栈）
+echo "net.ipv6.conf.all.disable_ipv6=1" >> package/base-files/files/etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6=1" >> package/base-files/files/etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6=1" >> package/base-files/files/etc/sysctl.conf
